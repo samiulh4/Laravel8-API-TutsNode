0096ea5a3af0b45e7133050a754a0f6b7886fa13
Md.Samiul  Haque <samiulh4@gmail.com>
2021-07-14 23:52:53 +0600

    completed phase1

--- apiphase1/routes/api.php
+++ apiphase1/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// API Phase 1
+Route::get("list-employees", [ApiController::class, "listEmployees"]);
+Route::get("single-employee/{id}", [ApiController::class, "getSingleEmployee"]);
+Route::post("add-employee", [ApiController::class, "createEmployee"]);
+Route::put("update-employee/{id}", [ApiController::class, "updateEmployee"]);
+Route::delete("delete-employee/{id}", [ApiController::class, "deleteEmployee"]);
