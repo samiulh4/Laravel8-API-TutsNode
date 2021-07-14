859b2ff11f9f146bbe4ca0bc0772cc176a7165a5
Md.Samiul  Haque <samiulh4@gmail.com>
2021-07-14 20:28:46 +0600

    completed 3-1

---
+++ apiphase1/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function createEmployee(Request $request){
+
+    }
+
+    public function listEmployees(){
+
+    }
+
+    public function getSingleEmployee($id){
+
+    }
+
+    public function updateEmployee(Request $request, $id){
+
+    }
+
+    public function deleteEmployee($id){
+
+    }
+}
